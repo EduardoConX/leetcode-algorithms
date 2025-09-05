ea8b1b764ac68026fcbaefb3be8c5feff78826ae
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2025-09-04 21:11:59 -0600

    Create layout to use in the whole site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
