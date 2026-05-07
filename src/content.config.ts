246dc7762a1f834a6f19dfe0a879c0a1a01f0c8d
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2026-05-06 20:45:26 -0600

    Update Astro version

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,15 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+import { glob } from "astro/loaders";
 
 const problems = defineCollection({
-  type: "content",
+  loader: glob({ base: "./src/content/problems", pattern: "**/*.mdx" }),
   schema: z.object({
     number: z.number().min(1),
     title: z.string(),
     difficulty: z.enum(["Easy", "Medium", "Hard"]),
     categories: z.array(z.string()),
+    slug: z.string(),
   }),
 });
 
